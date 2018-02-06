14e931aee4e6eff14b2ed15a9ffb733117e3a35a
Edward Ionides <ionides@umich.edu>
2018-02-06 18:09:38 -0500

    more on notes5

--- 05/notes05.R
+++ 05/notes05.R
@@ -1,3 +1,15 @@
 ## ----setup,echo=F,results=F,cache=F--------------------------------------
 library(broman) # used for myround 
 
+## ----reconstruct_variables,echo=F----------------------------------------
+L <- read.table(file="life_expectancy.txt",header=TRUE)
+L_fit <- lm(Total~Year,data=L)
+L_detrended <- L_fit$residuals
+U <- read.table(file="unemployment.csv",sep=",",header=TRUE)
+U_annual <- apply(U[,2:13],1,mean)
+U_detrended <- lm(U_annual~U$Year)$residuals
+L_detrended <- subset(L_detrended,L$Year %in% U$Year)
+
+## ----detrended_lm--------------------------------------------------------
+lm1 <- lm(L_detrended~U_detrended) ; summary(lm1)
+
