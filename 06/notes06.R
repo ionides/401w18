371a64ca61e503bfb13063a232d0361da0f28158
Edward L Ionides <ionides@umich.edu>
2018-03-02 09:33:29 -0500

    notes6 ctd

--- 06/notes06.R
+++ 06/notes06.R
@@ -19,18 +19,46 @@ z <- qnorm(1-0.05/2) # for a 95% CI using a normal approximation
 cat("CI = [", beta_U - z * SE_U, ",", beta_U + z * SE_U, "]")
 
 ## ----sim-----------------------------------------------------------------
-N <- 1000 ; sigma <- 1 ; d <- 10
+N <- 50000 ; sigma <- 1 ; d <- 10 ; set.seed(23)
 X <- matrix(rnorm(N*(d+1),mean=0,sd=sigma),nrow=N)
 
 ## ----T_eval--------------------------------------------------------------
 T_evaluator <- function(x) x[d+1] / sqrt(sum(x[1:d]^2)/d) 
 
 ## ----T_sim---------------------------------------------------------------
-T_simulated <- apply(X,1,T_evaluator)
+Tsim <- apply(X,1,T_evaluator)
 
-## ----T_plot,fig.width=4,fig.height=3,out.width="3in",echo=T,eval=F-------
-## hist(T_simulated)
+## ----T_plot_code,echo=T,eval=F-------------------------------------------
+## hist(Tsim,freq=F,main="",
+##   breaks=30,ylim=c(0,0.4))
+## x <- seq(length=200,
+##   min(Tsim),max(Tsim))
+## lines(x,dnorm(x),
+##   col="blue",
+##   lty="dashed")
+## lines(x,dt(x,df=d),
+##   col="red")
 
-## ------------------------------------------------------------------------
+## ----T_plot,echo=F,eval=T,fig.width=4,fig.height=3,out.width="2.5in"-----
+par(mai=c(0.8,0.8,0.1,0.1))
+hist(Tsim,freq=F,main="",
+  breaks=30,ylim=c(0,0.4))
+x <- seq(length=200,
+  min(Tsim),max(Tsim))
+lines(x,dnorm(x),
+  col="blue",
+  lty="dashed")
+lines(x,dt(x,df=d),
+  col="red")
 
+## ----range---------------------------------------------------------------
+range(Tsim)
+
+## ----tail_z--------------------------------------------------------------
+2*(1-pnorm(5))
+2*(1-pnorm(6))
+
+## ----tail_t--------------------------------------------------------------
+2*(1-pt(5,df=d))
+2*(1-pt(6,df=d))
 
